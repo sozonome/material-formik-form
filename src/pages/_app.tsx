83baf793a1b2b64adc7ba02ce12a98dbe6c92c92
Agustinus Nathaniel <berserk.nate@gmail.com>
2020-10-15 17:15:19 +0700

    Initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,35 @@
+import { useEffect } from "react";
+import { ThemeProvider, CssBaseline } from "@material-ui/core";
+import PropTypes from "prop-types";
+
+import Layout from "../components/layout";
+
+import theme from "../styles/theme";
+import "../styles/globals.css";
+
+const MyApp = ({ Component, pageProps }) => {
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <ThemeProvider theme={theme}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
+  );
+};
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
+
+export default MyApp;
